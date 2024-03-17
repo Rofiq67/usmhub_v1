49bba819d4539ec231b5b29ab7f38de5ddb838d3
Abdur  Rofiq <abdurrofiq67@gmail.com>
2024-03-18 00:22:06 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 // import 'package:usmhub_v1/features/aspirasi_page/presentations/pages/form_aspirasi1.dart';
 // import 'package:usmhub_v1/features/aspirasi_page/presentations/pages/form_aspirasi2.dart';
 // import 'package:usmhub_v1/features/aspirasi_page/presentations/pages/form_aspirasi3.dart';
-import 'package:usmhub_v1/features/aspirasi_page/presentations/pages/sent_aspirasi.dart';
+// import 'package:usmhub_v1/features/aspirasi_page/presentations/pages/sent_aspirasi.dart';
+// import 'package:usmhub_v1/features/registration_page/presentations/pages/login_page.dart';
+import 'package:usmhub_v1/features/registration_page/presentations/pages/signup_page.dart';
 // import 'package:usmhub_v1/features/pengaduan_page/presentations/pages/form_pengaduan4.dart';
 // import 'package:usmhub_v1/features/pengaduan_page/presentations/pages/form_pengaduan5.dart';
 // import 'package:usmhub_v1/features/pengaduan_page/presentations/pages/sent_pengaduan.dart';
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xffF5F5F5),
       ),
       debugShowCheckedModeBanner: false,
-      home: const SentAspirasi(),
+      home: const SignupPage(),
     );
   }
 }
