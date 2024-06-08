c9ef5f8f921f36ffa7159e85640cbc6f9b6d5257
Abdur  Rofiq <abdurrofiq67@gmail.com>
2024-06-09 00:29:14 +0700

    update baru history page

--- lib/features/progress_page/presentations/pages/progress_page.dart
+++ lib/features/progress_page/presentations/pages/progress_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:usmhub_v1/features/aspirasi_page/data/models/aspirasi_models.dart';
+import 'package:usmhub_v1/features/pengaduan_page/data/models/aduan_models.dart';
 import 'package:usmhub_v1/features/progress_page/domains/controllers/progress_controller.dart';
 import 'package:usmhub_v1/features/progress_page/presentations/widgets/card_progress.dart';
 
@@ -26,20 +28,20 @@ class ProgressPage extends StatelessWidget {
           return const Center(child: Text('Belum ada riwayat.'));
         }
 
-        var sedangBerjalanAduan = progressController.riwayatAduan
-            .where((aduan) =>
-                aduan.status == 'Belum Dibaca' ||
-                aduan.status == 'Ditindaklanjuti')
-            .toList();
-        var selesaiAduan = progressController.riwayatAduan
-            .where((aduan) => aduan.status == 'Selesai')
-            .toList();
-        var sedangBerjalanAspirasi = progressController.riwayatAspirasi
-            .where((aspirasi) => aspirasi.status == 'Belum Dibaca')
-            .toList();
-        var selesaiAspirasi = progressController.riwayatAspirasi
-            .where((aspirasi) => aspirasi.status == 'Telah diterima')
-            .toList();
+        var sedangBerjalan = [
+          ...progressController.riwayatAduan.where((aduan) =>
+              (aduan).status == 'Belum Dibaca' ||
+              aduan.status == 'Ditindaklanjuti'),
+          ...progressController.riwayatAspirasi
+              .where((aspirasi) => (aspirasi).status == 'Belum Dibaca')
+        ];
+
+        var selesai = [
+          ...progressController.riwayatAduan
+              .where((aduan) => (aduan).status == 'Selesai'),
+          ...progressController.riwayatAspirasi
+              .where((aspirasi) => (aspirasi).status == 'Telah diterima')
+        ];
 
         return RefreshIndicator(
           onRefresh: _refreshHistory,
@@ -61,7 +63,7 @@ class ProgressPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 32),
                 const Text(
-                  'Sedang berjalan - Aduan',
+                  'Sedang Berjalan',
                   style: TextStyle(
                     color: Color(0xFF1C1C1C),
                     fontSize: 24,
@@ -72,18 +74,33 @@ class ProgressPage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 16),
-                ...sedangBerjalanAduan.map((aduan) => CardProgress(
-                      iconCard: Iconsax.microphone,
-                      colorIconCard: const Color(0xff3E4095),
-                      bgIconCard: const Color(0xFFBCBEF3),
-                      jdlCard: aduan.jenisPengaduan,
-                      stsCard: aduan.status,
-                      dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
-                          .format(aduan.createdAt),
-                    )),
+                ...sedangBerjalan.map((item) {
+                  var jenis = item is Aduan
+                      ? item.jenisPengaduan
+                      : (item as Aspirasi).jenisAspirasi;
+                  var status =
+                      item is Aduan ? item.status : (item as Aspirasi).status;
+                  var createdAt = item is Aduan
+                      ? item.createdAt
+                      : (item as Aspirasi).createdAt;
+                  return CardProgress(
+                    iconCard:
+                        item is Aduan ? Iconsax.microphone : Iconsax.lamp_on5,
+                    colorIconCard: item is Aduan
+                        ? const Color(0xff3E4095)
+                        : const Color(0xffff8800),
+                    bgIconCard: item is Aduan
+                        ? const Color(0xFFBCBEF3)
+                        : const Color(0xffFEFCB9),
+                    jdlCard: jenis,
+                    stsCard: status,
+                    dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
+                        .format(createdAt),
+                  );
+                }),
                 const SizedBox(height: 32),
                 const Text(
-                  'Sedang berjalan - Aspirasi',
+                  'Selesai',
                   style: TextStyle(
                     color: Color(0xFF1C1C1C),
                     fontSize: 24,
@@ -94,59 +111,30 @@ class ProgressPage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 16),
-                ...sedangBerjalanAspirasi.map((aspirasi) => CardProgress(
-                      iconCard: Iconsax.microphone,
-                      colorIconCard: const Color(0xff3E4095),
-                      bgIconCard: const Color(0xFFBCBEF3),
-                      jdlCard: aspirasi.jenisAspirasi,
-                      stsCard: aspirasi.status,
-                      dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
-                          .format(aspirasi.createdAt),
-                    )),
-                const SizedBox(height: 32),
-                const Text(
-                  'Selesai - Aduan',
-                  style: TextStyle(
-                    color: Color(0xFF1C1C1C),
-                    fontSize: 24,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    height: 0,
-                    letterSpacing: 0.48,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                ...selesaiAduan.map((aduan) => CardProgress(
-                      iconCard: Iconsax.microphone,
-                      colorIconCard: const Color(0xff3E4095),
-                      bgIconCard: const Color(0xFFBCBEF3),
-                      jdlCard: aduan.jenisPengaduan,
-                      stsCard: aduan.status,
-                      dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
-                          .format(aduan.createdAt),
-                    )),
-                const SizedBox(height: 32),
-                const Text(
-                  'Selesai - Aspirasi',
-                  style: TextStyle(
-                    color: Color(0xFF1C1C1C),
-                    fontSize: 24,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    height: 0,
-                    letterSpacing: 0.48,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                ...selesaiAspirasi.map((aspirasi) => CardProgress(
-                      iconCard: Iconsax.microphone,
-                      colorIconCard: const Color(0xff3E4095),
-                      bgIconCard: const Color(0xFFBCBEF3),
-                      jdlCard: aspirasi.jenisAspirasi,
-                      stsCard: aspirasi.status,
-                      dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
-                          .format(aspirasi.createdAt),
-                    )),
+                ...selesai.map((item) {
+                  var jenis = item is Aduan
+                      ? item.jenisPengaduan
+                      : (item as Aspirasi).jenisAspirasi;
+                  var status =
+                      item is Aduan ? item.status : (item as Aspirasi).status;
+                  var createdAt = item is Aduan
+                      ? item.createdAt
+                      : (item as Aspirasi).createdAt;
+                  return CardProgress(
+                    iconCard:
+                        item is Aduan ? Iconsax.microphone : Iconsax.lamp_on5,
+                    colorIconCard: item is Aduan
+                        ? const Color(0xff3E4095)
+                        : const Color(0xffff8800),
+                    bgIconCard: item is Aduan
+                        ? const Color(0xFFBCBEF3)
+                        : const Color(0xffFEFCB9),
+                    jdlCard: jenis,
+                    stsCard: status,
+                    dateCard: DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
+                        .format(createdAt),
+                  );
+                }),
                 const SizedBox(height: 16),
               ],
             ),
